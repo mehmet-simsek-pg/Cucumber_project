d041cf758b17e91b4864e8d1322e5b3e4a79b7e7
Mehmet Simsek <simsekmehmet00@gmail.com>
2025-10-20 18:53:55 +0200

    LoginPage created

---
+++ src/main/java/pages/LoginPage.java
@@ -0,0 +1,21 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends BasePage{
+
+    @FindBy(css = "input[formcontrolname='username']")
+    private WebElement emailInput;
+
+    @FindBy(css = "input[formcontrolname='password']")
+    private WebElement passwordInput;
+
+    @FindBy(css = "button[aria-label='LOGIN']")
+    private WebElement loginBtn;
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+}
