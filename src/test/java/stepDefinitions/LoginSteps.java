925fb4b6dbbe478a0e87e0b5ff48d382da5b1620
Mehmet Simsek <simsekmehmet00@gmail.com>
2025-10-20 20:17:27 +0200

    LoginSteps updated

--- src/test/java/stepDefinitions/LoginSteps.java
+++ src/test/java/stepDefinitions/LoginSteps.java
@@ -5,5 +5,24 @@ import io.cucumber.java.en.When;
 
 public class LoginSteps {
 
-
+    @When("Navigate to the webpage")
+    public void navigate_to_the_webpage() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("Enter username and password")
+    public void enter_username_and_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("Click login button")
+    public void click_login_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("Verify Home page opened")
+    public void verify_home_page_opened() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 }
