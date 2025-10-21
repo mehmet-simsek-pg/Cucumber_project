7f5facb3cd7b8f4cfa91d8c0b5d89f05446d0a6d
Mehmet Simsek <simsekmehmet00@gmail.com>
2025-10-21 20:38:25 +0200

    HomePage updated

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -9,11 +9,18 @@ public class HomePage extends BasePage{
     @FindBy(css = "div[data-testid='patient-queue-header']>div>div>p:nth-child(1)")
     private WebElement clinicText;
 
+    @FindBy(css = "button[name='AddPatientIcon']")
+    private WebElement addPatientBtn;
+
     public HomePage(WebDriver driver) {
         super(driver);
     }
 
     public void verifyHomePage() {
         verifyDisplayed(clinicText, "Clinic");
+    }
+
+    public void clickAddPatientBtn() {
+        clickElement(addPatientBtn);
     }
 }
