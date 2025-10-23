229d2b70b50c258ab7d228b6fe4374fdf9e020de
Mehmet Simsek <simsekmehmet00@gmail.com>
2025-10-23 19:40:12 +0200

    DataTableSteps updated

--- src/test/java/stepDefinitions/DataTableSteps.java
+++ src/test/java/stepDefinitions/DataTableSteps.java
@@ -1,33 +1,35 @@
 package stepDefinitions;
 
-import hooks.Hooks;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.datatable.DataTable;
-import org.openqa.selenium.WebDriver;
-import pages.DataTablePage;
 import pojo.User;
 
 import java.util.List;
+import java.util.Map;
 
 public class DataTableSteps {
 
-    WebDriver driver;
-    DataTablePage dataTablePage;
-
     @Given("User is on login page")
     public void user_is_on_login_page() {
-       //driver = Hooks.driver;
-       //dataTablePage = new DataTablePage(driver);
         System.out.println("Ilk step calisti");
     }
 
     @Then("User enters credentials")
     public void user_enters_credentials(DataTable dataTable) {
 
+        /** POJO ile datatable kullanimi
         List<User> users = dataTable.asList(User.class);
         for (User user:users) {
             System.out.println(user.getUsername() + " " + user.getPassword() );
+        }
+         */
+        List<Map<String,String>> users = dataTable.asMaps(String.class, String.class);
+
+        for (Map<String, String> user : users) {
+            String username = user.get("username");
+            String password = user.get("password");
+            System.out.println(username + " " + password);
         }
     }
 }
