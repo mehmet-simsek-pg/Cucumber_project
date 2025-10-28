459ad1fda42776ca09b04f3d465d805643805df4
Mehmet Simsek <simsekmehmet00@gmail.com>
2025-10-28 18:51:28 +0100

    update pom.xml

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -4,10 +4,12 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "src/test/resources/features/patientList.feature", // features dosyalarinin yolu
+        features = "src/test/resources/features/login.feature", // features dosyalarinin yolu
         // tüm feature dosyalarini calistirabilirim, ya da sectigim feature dosyalarini calistirabilirim
         glue = {"stepDefinitions", "hooks"}, // Java kodlarinin oldugu klasörler
         plugin = {"pretty", "html:target/cucumber-report.html"}
+        // {"json:target/cucumber-report.json"}
+        // {"junit:target/cucumber-report.xml"}
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
 }
